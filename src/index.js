fdf166adfb38f63c97ff26204b24e81b3054a220
CamiloTaborda <121066654+CamiloTaborda@users.noreply.github.com>
2023-05-24 14:11:46 -0500

    firts commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+// import ReactDOM from "react-dom";
+import App from "./routes/App";
+import { createRoot } from 'react-dom/client';
+
+createRoot(document.getElementById('app')).render(<App />);
+
+
+// ReactDOM.render(<App />, document.getElementById("app"));
+
